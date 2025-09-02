2a9161cf6a2cbc53864ea1f73ef7d9a00b371976
Aman Singh <amansingh.makemeup@gmail.com>
2025-09-02 20:20:15 +0530

    changes

--- Practice Questions/11.Missing_Numbers.cpp
+++ Practice Questions/11.Missing_Numbers.cpp
@@ -1,13 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int missing(vector<int> v1,int N){
-    int n = N-1;
     int xor1=0,xor2 =0;
-    for(int i =0;i<n;i++){
+    for(int i =0;i<N;i++){
         xor2=xor2^v1[i];
         xor1=xor1^(i+1);
     }
-    xor1=xor1^N;
     return xor1^xor2;
 
 }
